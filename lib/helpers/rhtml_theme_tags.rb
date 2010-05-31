aeb11b61ccc09e4885ceb383b055c397c1d9ae7d
James Stewart <james@ketlai.co.uk>
2010-05-31 11:56:16 +0100

    Delegate production of javascript/stylesheet include tags to the core rails helpers. Should resolve problems with html_safe

--- lib/helpers/rhtml_theme_tags.rb
+++ lib/helpers/rhtml_theme_tags.rb
@@ -20,16 +20,13 @@ module ActionView::Helpers::AssetTagHelper
       theme = theme || controller.current_theme
       compute_public_path(source, "themes/#{theme}/javascript", 'js')
    end
-   
 
    # This tag it will automatially include theme specific css files
    def theme_stylesheet_link_tag(*sources)
       sources.uniq!
       options = sources.last.is_a?(Hash) ? sources.pop.stringify_keys : { }
-      sources.collect { |source|
-         source = theme_stylesheet_path(source)
-         tag("link", { "rel" => "Stylesheet", "type" => "text/css", "media" => "screen", "href" => source }.merge(options))
-      }.join("\n")
+      themed_sources = sources.collect { |source| theme_stylesheet_path(source) }
+      stylesheet_link_tag(themed_sources)
    end
    
    # This tag will return a theme-specific IMG
@@ -47,20 +44,21 @@ module ActionView::Helpers::AssetTagHelper
      tag("img", options)
    end
    
-   # This tag can be used to return theme-specific javscripts
+   # This tag can be used to return theme specific javascript files
    def theme_javascript_include_tag(*sources)
      options = sources.last.is_a?(Hash) ? sources.pop.stringify_keys : { }
+
      if sources.include?(:defaults)        
-       sources = sources[0..(sources.index(:defaults))] + 
-         @@javascript_default_sources.dup + 
-         sources[(sources.index(:defaults) + 1)..sources.length]
+       pre_default_sources = sources[0..(sources.index(:defaults))]
+       post_default_sources = sources[(sources.index(:defaults) + 1)..sources.length]
+       sources = pre_default_sources + @@javascript_default_sources.dup + post_default_sources
        sources.delete(:defaults) 
-       sources << "application" if defined?(RAILS_ROOT) && File.exists?("#{RAILS_ROOT}/public/javascripts/application.js") 
+       
+       sources << "application" if defined?(Rails.root) && File.exists?("#{Rails.root}/public/javascripts/application.js") 
      end
-     sources.collect { |source|
-       source = theme_javascript_path(source)        
-       content_tag("script", "", { "type" => "text/javascript", "src" => source }.merge(options))
-     }.join("\n")
+
+     themed_sources = sources.collect { |source| theme_javascript_path(source) }
+     javascript_include_tag(themed_sources)
    end
 
 end
